44722796484ee6f3405a2ce28424f888f3ad7cd9
dlukas1 <dlukas@itcollege.ee>
2017-01-10 16:24:17 +0200

    3rd

--- src/absVSumma.java
+++ src/absVSumma.java
@@ -1,37 +1,27 @@
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
-public class tests {
+public class absVSumma {
 
     ///*Koostage Java-meetod, mis leiab etteantud massiivi m elementide absoluutv22rtuste summa.
     public static void main(String[] args) {
 
+        //создаем массив
         int myArray[] = {3, 5, 7, -12};
         System.out.println("AbsVaartSumma = " + absvSumma(myArray));
     }
 
+    //метод принимает массив
     public static int absvSumma (int[] m) {
 
         int sum = 0;
-        int absint = 0;
+        int absint;
 
+        //for - берет по очереди каждое число массива, абс и складывает в сум
         for(int i = 0; i < m.length; i++)
         {
             absint = Math.abs(m[i]);
             sum = sum + absint;
         }
         return sum;
+    }
+
+
 }
-    }
-
-    /* InStreams - поток, здесь считаем сумму массива
-    public static int absvSumma(int[] myArray) {
-
-        int total = IntStream.of(myArray).sum();
-
-        return total;
-    }
-     */
-
-
-
